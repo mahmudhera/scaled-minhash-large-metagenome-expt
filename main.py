3c3e948812b1892f24e221f6b4a3b91d5a754bfb
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2021-12-07 14:23:31 -0500

    added file

---
+++ main.py
@@ -0,0 +1,42 @@
+import screed
+import subprocess
+import mmh3
+import numpy as np
+
+def get_kmers_in_file(filename, k):
+	# run jellyfish
+	cmd = "jellyfish count -m " + str(k) + " -s 2G -t 32 -o tmp -C -L 1 -U 99999999 " + filename
+	args = cmd.split(' ')
+	subprocess.call(args)
+	cmd = "jellyfish dump -L 1 -U 99999999 -o tmp-dump -c tmp"
+	args = cmd.split(' ')
+	subprocess.call(args)
+	# open output file
+	# construct set
+	# return the set
+	
+
+# c is float, 0 < c < 1
+def generate_c_percent_of_file(c, genome_filename, out_filename):
+    subprocess.call(['rm', out_filename])
+    length = 0
+    with screed.open(genome_filename) as f:
+        for record in f:
+            length += len(record.sequence)
+    required_length = int(length * c)
+    with screed.open(genome_filename) as f:
+        for record in f:
+            if len(record.sequence) > required_length:
+                small_str = record.sequence[:required_length]
+                break
+    f2 = open(out_filename, 'w')
+    f2.write('>small_seq\n')
+    f2.write(small_str)
+    f2.write('\n')
+    f2.close()
+	
+	
+if __name__ == "__main__":
+	mg_filename = "reads_file.fastq"
+	
+	s = get_kmers_in_file(mg_filename)
