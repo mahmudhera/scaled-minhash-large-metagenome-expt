27ce6f8b61921bd655827294d2e76537d4e2a080
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2021-12-09 04:10:49 -0500

    added file

--- main.py
+++ main.py
@@ -136,93 +136,94 @@ def create_super_metagenome(metagenome_filename, small_genome_filename, super_mg
     f = open(super_mg_filename, 'w')
     subprocess.call(args, stdout=f)
     f.close()
-	
-if __name__ == "__main__":
 
-	f = open('results', 'w')
 
-	#mg_filename = "reads_file.fastq"
-	mg_filename = 'SRR492190.contigs.fa'
-	g_filename = 'staphylococcus.fasta'
-	smallg_filename = 'temp.fasta'
-	k = 21
-	#containment_ranges = [0.01] + [0.1*i for i in range(1, 10)] + [0.99]
-	containment_ranges = [0.2]
-	scale_facor = 0.0005
-	num_runs = 2
-	seeds = [i for i in range(num_runs)]
-	seed = 1
+
+
+f = open('results', 'w')
+
+#mg_filename = "reads_file.fastq"
+mg_filename = 'SRR492190.contigs.fa'
+g_filename = 'staphylococcus.fasta'
+smallg_filename = 'temp.fasta'
+k = 21
+#containment_ranges = [0.01] + [0.1*i for i in range(1, 10)] + [0.99]
+containment_ranges = [0.2]
+scale_facor = 0.0005
+num_runs = 2
+seeds = [i for i in range(num_runs)]
+seed = 1
+
+kmers_in_metagenome = get_kmers_in_file(mg_filename, k)
+kmers_in_genome = get_kmers_in_file(g_filename, k)
+
+sketches_metagenome = {}
+print('generating sketches for metagenome')
+for seed in seeds:
+	sketches_metagenome[seed] = create_scaled_minhash(kmers_in_metagenome, seed, scale_facor)
+print('sketch_sizes:')
+print([sketches_metagenome[seed].get_sketch_size() for seed in seeds])
+
+sketch_metagenome = sketches_metagenome[1]
+
+sketches_genome = {}
+print('generating sketches for kmers in genome')
+for seed in seeds:
+	sketches_genome[seed] = create_scaled_minhash(kmers_in_genome, seed, scale_facor)
+print('sketch sizes:')
+print([sketches_genome[seed].get_sketch_size() for seed in seeds])
+
+sketch_genome = sketches_genome[1]
+
+all_hashes_metagenome = ScaledMinHash(1.0, 2**64)
+add_kmers_in_scaled_minhash(kmers_in_metagenome, all_hashes_metagenome, 0)
+
+all_hashes_genome = ScaledMinHash(1.0, 2**64)
+add_kmers_in_scaled_minhash(kmers_in_genome, all_hashes_genome, 0)
+
+for C in containment_ranges:
+	print('C: ' + str(C))
+	generate_c_percent_of_file(C, g_filename, smallg_filename)
+	kmers_in_small_portion = get_kmers_in_file(smallg_filename, k)
+	#print('kmers in small part: ' + str(len(kmers_in_small_portion)))
 	
-	kmers_in_metagenome = get_kmers_in_file(mg_filename, k)
-	kmers_in_genome = get_kmers_in_file(g_filename, k)
+	all_hashes_super_mg = ScaledMinHash(1.0, 2**64, all_hashes_metagenome.hash_set)
+	add_kmers_in_scaled_minhash(kmers_in_small_portion, all_hashes_super_mg, 0)
+	true_c = all_hashes_genome.get_containment(all_hashes_super_mg)
+	print('true containment: ' + str(true_c))
 	
-	sketches_metagenome = {}
-	print('generating sketches for metagenome')
+	print('now doing mash runs..')
+	smg_filename = 'supermetagenome.fasta'
+	create_super_metagenome(mg_filename, smallg_filename, smg_filename)
+	unique_kmers_union = set(kmers_in_genome + kmers_in_metagenome + kmers_in_small_portion)
+	size_union = len(unique_kmers_union)
+	sketch_size = sketch_genome.get_sketch_size()
+	size_genome = len(set(kmers_in_genome))
+	mash_containments = get_mash_containments(g_filename, smg_filename, sketch_size, size_union, size_genome, num_runs)
+	
+	print(mash_containments)
+	
+	scaled_containments = []
 	for seed in seeds:
-		sketches_metagenome[seed] = create_scaled_minhash(kmers_in_metagenome, seed, scale_facor)
-	print('sketch_sizes:')
-	print([sketches_metagenome[seed].get_sketch_size() for seed in seeds])
+		sketch_genome = sketches_genome[seed]
+		sketch_metagenome = sketches_metagenome[seed]
 	
-	sketch_metagenome = sketches_metagenome[1]
+		sketch_added = ScaledMinHash( sketch_metagenome.scale_factor, sketch_metagenome.H, sketch_metagenome.hash_set )
+		add_kmers_in_scaled_minhash(kmers_in_small_portion, sketch_added, seed)
+		#print('added kmers in small genome in sketch. new sletch size:')
+		#print(sketch_added.get_sketch_size())
+		
+		#print("seed: " + str(seed))
+		#print('for this seed, containment is: ')
+		sc_c = sketch_genome.get_scaled_containment(sketch_added)
+		scaled_containments.append(sc_c)
+		print(seed, sc_c)
+		#print(sketch_added.get_scaled_containment(sketch_genome))
 	
-	sketches_genome = {}
-	print('generating sketches for kmers in genome')
-	for seed in seeds:
-		sketches_genome[seed] = create_scaled_minhash(kmers_in_genome, seed, scale_facor)
-	print('sketch sizes:')
-	print([sketches_genome[seed].get_sketch_size() for seed in seeds])
+	print(C, scaled_containments)
+	string_list = [str(C), str(true_c), str(np.average(mash_containments)), str(np.var(mash_containments)), str(np.average(scaled_containments)), str(np.var(scaled_containments))]
+	f.write(','.join(string_list))
+	f.write('\n')
+	f.flush()
 	
-	sketch_genome = sketches_genome[1]
-	
-	all_hashes_metagenome = ScaledMinHash(1.0, 2**64)
-	add_kmers_in_scaled_minhash(kmers_in_metagenome, all_hashes_metagenome, 0)
-	
-	all_hashes_genome = ScaledMinHash(1.0, 2**64)
-	add_kmers_in_scaled_minhash(kmers_in_genome, all_hashes_genome, 0)
-	
-	for C in containment_ranges:
-		print('C: ' + str(C))
-		generate_c_percent_of_file(C, g_filename, smallg_filename)
-		kmers_in_small_portion = get_kmers_in_file(smallg_filename, k)
-		#print('kmers in small part: ' + str(len(kmers_in_small_portion)))
-		
-		all_hashes_super_mg = ScaledMinHash(1.0, 2**64, all_hashes_metagenome.hash_set)
-		add_kmers_in_scaled_minhash(kmers_in_small_portion, all_hashes_super_mg, 0)
-		true_c = all_hashes_genome.get_containment(all_hashes_super_mg)
-		print('true containment: ' + str(true_c))
-		
-		print('now doing mash runs..')
-		smg_filename = 'supermetagenome.fasta'
-		create_super_metagenome(mg_filename, smallg_filename, smg_filename)
-		unique_kmers_union = set(kmers_in_genome + kmers_in_metagenome + kmers_in_small_portion)
-		size_union = len(unique_kmers_union)
-		sketch_size = sketch_genome.get_sketch_size()
-		size_genome = len(set(kmers_in_genome))
-		mash_containments = get_mash_containments(g_filename, smg_filename, sketch_size, size_union, size_genome, num_runs)
-		
-		print(mash_containments)
-		
-		scaled_containments = []
-		for seed in seeds:
-			sketch_genome = sketches_genome[seed]
-			sketch_metagenome = sketches_metagenome[seed]
-		
-			sketch_added = ScaledMinHash( sketch_metagenome.scale_factor, sketch_metagenome.H, sketch_metagenome.hash_set )
-			add_kmers_in_scaled_minhash(kmers_in_small_portion, sketch_added, seed)
-			#print('added kmers in small genome in sketch. new sletch size:')
-			#print(sketch_added.get_sketch_size())
-			
-			#print("seed: " + str(seed))
-			#print('for this seed, containment is: ')
-			sc_c = sketch_genome.get_scaled_containment(sketch_added)
-			scaled_containments.append(sc_c)
-			print(seed, sc_c)
-			#print(sketch_added.get_scaled_containment(sketch_genome))
-		
-		print(C, scaled_containments)
-		string_list = [str(C), str(true_c), str(np.average(mash_containments)), str(np.var(mash_containments)), str(np.average(scaled_containments)), str(np.var(scaled_containments))]
-		f.write(','.join(string_list))
-		f.write('\n')
-		f.flush()
-		
-	f.close()
+f.close()
