818a6c96c01a7e380f57ce7865c0ec5bd4978f0d
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2021-12-09 00:44:46 -0500

    added file

--- main.py
+++ main.py
@@ -152,6 +152,7 @@ if __name__ == "__main__":
 			sc_c = sketch_added.get_containment(sketch_genome)
 			scaled_containments.append(sc_c)
 			print(sc_c)
+			print(sketch_genome.get_containment(sketch_added))
 		
 		print(C, scaled_containments)
 		
